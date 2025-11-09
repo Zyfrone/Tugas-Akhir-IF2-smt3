b64d50f6cad22eb8c69ff8041faf1ff79370b7da
Zyfrone <zyfronpr@gmail.com>
2025-11-09 20:54:25 +0700

    Add files via upload

--- js/confirm.js
+++ js/confirm.js
@@ -1,6 +1,15 @@
-function confirmSubmit() {
-                let result = confirm('Apakah kamu yakin?');
-                if (result) {
-                    window.location.href = "index.html";
-                }
-            }
+function confirmSubmit(event) {
+    event.preventDefault();
+    
+    const form = event.target;
+
+    if (!form.checkValidity()) {
+        return; 
+    }
+    
+    let result = confirm('Apakah kamu yakin ingin mengirim saran ini?');
+    
+    if (result) {
+        form.submit();
+    } 
+}
