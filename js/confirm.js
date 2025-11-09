9d43e5a92ec3d45b3343b30fea2d5a41d639fd1e
Zyfrone <zyfronpr@gmail.com>
2025-11-09 19:16:27 +0700

    Add files via upload

---
+++ js/confirm.js
@@ -0,0 +1,6 @@
+function confirmSubmit() {
+                let result = confirm('Apakah kamu yakin?');
+                if (result) {
+                    window.location.href = "index.html";
+                }
+            }
